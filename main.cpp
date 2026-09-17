8abd629afc43803b451dc70faa34a59e0d7f4a6f
hihikomori <ghhhhhnika@gmail.com>
2026-09-17 20:18:12 +0300

    clicker

--- main.cpp
+++ main.cpp
@@ -1,2 +1,23 @@
+#include <chrono>
+
+class Clicker
+{
+public:
+  Clicker():
+    start_(std::chrono::high_resolution_clock::now())
+  {}
+  double millisec() const
+  {
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
+    auto t = high_resolution_clock::now();
+    return duration_cast< milliseconds >(t - start_).count();
+  }
+
+private:
+  std::chrono::time_point< std::chrono::system_clock > start_;
+};
+
 int main()
 {}
